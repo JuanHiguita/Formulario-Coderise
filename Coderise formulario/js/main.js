60ffa88b3415a36b4142ab50f2937cea5c62a33b
juanse01 <juansehiguita@gmail.com>
2018-06-01 19:41:15 -0500

    Add files via upload
    --> validacion de los campos necesarios en la base de datos
    --> funcionalidad para el boton "guardar"
    --> alerta de "error" en caso de que el usuario no rellene los parametros adecuadamente

--- Coderise formulario/js/main.js
+++ Coderise formulario/js/main.js
@@ -5,33 +5,118 @@ $('#guardar').hide();
 //le decimos al programa que al hacer click nos muestre los campos que habiamos ocultado y que oculte los de la pagina principal
 $('#Formulario').click(function(){
     $('#Formulario').hide(),
+    $('#text-otro').hide(),
     $('#login').hide(),
     $('#campos_formulario').show(),
     $('#div_fechaSolicitud').show(),
     $('#guardar').show();
+
 });
 
-//escribir en la base de datos 
-$('#guardar').click(function(){
-var datos_form= {
+function validar(){
+ /*creo una variable de tipo booleano que en principio tendrá un valor true(verdadero),
+ y que se convertirá en false(falso) cuando la condición no se cumpla
+*/
+    var correcto = true;
+    var mensaje = "Debes de corregir los siguientes errores: \n";
+    /*
+    ahora revisaremos que los parametros pedidos al usuario se cumplan y de no ser asi la variable
+    creada anteriormente cambiara su valor a "false"(falso), en este caso decimos que si el nombre
+    contiene menos de 5 caracteres no es un nombre valido
+    */
+    if ($('#txt-name').val().length < 5){
+        correcto = false;
+        mensaje += "--> El nombre insertado no es valido \n";
+    }
+    //Ahora repetimos lo mismo pero con la direccion
+    if ($('#text-direccion').val().length < 8){
+        correcto = false;
+        mensaje += "--> La direccion insertada no es valida \n";
+    }
+    /*Ahora validaremos que los siguiente solo sea una respuesta
+    numerica, para eso usaremos la funcion isNaN()
+    */ 
+    if(isNaN($('#text-pregunta2').val())){
+        correcto = false;
+        mensaje += "--> La respuesta insertada no es valida \n";
+    }
+    if(isNaN($('#text-telefono').val())){
+        correcto = false;
+        mensaje += "--> El numero telefonico insertado no es valido \n";
+    }
+    if(isNaN($('#text-pregunta3').val())){
+        correcto = false;
+        mensaje += "--> La respuesta insertada no es valida \n";
+    }
+    if(isNaN($('#text-pregunta4').val())){
+        correcto = false;
+        mensaje += "--> La respuesta insertada no es valida \n";
+    }
+    /*Para comprobar el email haremos uso de una expresión regular. Esto es una secuencia
+    de caracteres que nos dirá si el valor ingresado por el usuario tiene estructura de
+    correo electrónico. Lo que hacemos es obtener el value del campo de texto destinado
+    al email, y le aplicamos el método test() del objeto global RegExp(que nos permite
+    trabajar con expresiones regulares).*/
+    var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+    var email = $('#email-correo').val();
+    if (!expresion.test(email)){
+        correcto = false;
+        mensaje += "--> La direccion de correo no es valida \n";
+    }
+    /*Para validar el departamento y la ciudad debemos añadir un value distinto a cada "option". En el
+    código, he asignado un "value" con  valor "seleccion-d" para el primer option de Departamentos y un 
+    "value" con valor "seleccion-c" para el primer option de Ciudades. Los siguientes,
+    al no estar definidos toman el valor por defecto. Por tanto, si todos tienen value,
+    lo único que tenemos que comprobar es que estos no sean "seleccion-d" o "seleccion-c". Si es uno de estos,
+    correcto será false.*/
+    if($('#departamentos').val() == 'seleccion-d'){
+        correcto = false;
+        mensaje += "--> Debes de seleccionar un departamento \n";
+    }
+    if($('#ciudades').val() == 'seleccion-c'){
+        correcto = false;
+        mensaje += "--> Debes seleccionar una ciudad \n";
+    }
+    /*aqui mandaremos un aviso para el usuario, si no está todo bién, osea, si la variable
+    "correcto" ha devuelto "false" al menos una vez, generaremos una alerta advirtiendo
+    al usuario de que algunos datos ingresados no son los esperados.*/
+    if(!correcto){
+    alert(mensaje);
+    }
+    /*creamos un condicional en el cual si el valor de "correcto" es true nos permita 
+    guardar la informacion que luego mandaremos a la base de datos*/
+    if (correcto == true){    
+    var datos_form= {
         nombre:$('#txt-name').val(),
         fecha_nacimiento:$('#date-nacimiento').val(),
         genero:$('#genero').val(),
         email:$('#email-correo').val(),
-        telefono:$('#txt-telefono').val(),
+        telefono:$('#text-telefono').val(),
         departamento:$('#departamentos').val(),
         ciudad:$('#ciudades').val(),
-        tipo_domicilio:$('#inlineRadio-domicilio').val(),
-        direccion:$('#txt-direccion').val(),
-        ingresos:$('#txt-pregunta2').val(),
-        ingresos_mascota:$('#txt-pregunta3').val(),
+        tipo_domicilio:$('#tipo-domicilio').val(),
+        direccion:$('#text-direccion').val(), 
+        ingresos:$('#text-pregunta2').val(),
+        ingresos_mascota:$('#text-pregunta3').val(),
         experiencia:$('#inlineRadio-experiencia').val(),
         tiempo_mascota:$('#text-pregunta4').val(),  
-   };
-   console.log(datos_form);
-   firebase.database().ref('/datos_usuario')
-   .push(datos_form);
+        };
+        console.log(datos_form);
+        firebase.database().ref('/datos_usuario')
+        .push(datos_form);
+    }
+
+    return correcto;
+};
+//le indicamos al codigo que cuando se oprima el boton "guardar" ejecute la funcion "validar"
+$('#guardar').click(function(){
+    validar();
 });
+
+
+
+ 
+
 
 
 
